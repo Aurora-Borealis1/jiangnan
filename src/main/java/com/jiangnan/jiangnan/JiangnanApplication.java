de8b28c258825d9bb7b1af7d48a25d2833965f02
e <1965460697@qq.com>
2020-06-08 18:19:56 +0800

    first commit

---
+++ src/main/java/com/jiangnan/jiangnan/JiangnanApplication.java
@@ -0,0 +1,13 @@
+package com.jiangnan.jiangnan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiangnanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiangnanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiangnan/jiangnan/JiangnanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangnan.jiangnan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiangnanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
